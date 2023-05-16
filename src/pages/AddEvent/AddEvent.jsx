ad6c00741d5648fa4e584ad641ca16dd2719551c
Delowar Hossain <bscdelowar@yahoo.com>
2023-05-17 01:51:26 +0600

    add event done

--- src/pages/AddEvent/AddEvent.jsx
+++ src/pages/AddEvent/AddEvent.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 
 const AddEvent = () => {
+
+    const handleAddEvent=(event)=>{
+        event.preventDefault()
+        const form=event.target
+        const title=form.title.value
+        const date=form.date.value
+        const description=form.description.value
+        const image=form.image.value
+        const eventData={
+            title,date,description,image
+        }
+        console.log(eventData)
+        fetch('http://localhost:5000/events',{
+            method:'POST',
+            headers:{
+                'Content-Type':"application/json"
+            },
+            body:JSON.stringify(eventData)
+        }).then(res=>res.json())
+        .then(data=>{
+            if(data.insertedId){
+                alert('Inserted successfully')
+                form.reset()
+            }
+        })
+    }
     return (
         <div>
             <h1 className='bg-white p-10 text-2xl'>Add An Event</h1>
@@ -8,38 +34,39 @@ const AddEvent = () => {
                 <div className="lex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full  bg-base-100">
                         <div className="card-body w-full">
-                            <div className='flex gap-5'>
-                                <div className="form-control w-full">
-                                    <label className="label">
-                                        <span className="label-text">Event Title</span>
-                                    </label>
-                                    <input type="text" placeholder="email" className="input input-bordered" />
+                            <form onSubmit={handleAddEvent}>
+                                <div className='flex gap-5'>
+                                    <div className="form-control w-full">
+                                        <label className="label">
+                                            <span className="label-text">Event Title</span>
+                                        </label>
+                                        <input type="text" name="title" placeholder="title" className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control w-full">
+                                        <label className="label">
+                                            <span className="label-text">Event Date</span>
+                                        </label>
+                                        <input type="date" name="date" className="input input-bordered" />
+                                    </div>
                                 </div>
-                                <div className="form-control w-full">
-                                    <label className="label">
-                                        <span className="label-text">Event Date</span>
-                                    </label>
-                                    <input type="text" placeholder="email" className="input input-bordered" />
+                                <div className='flex gap-5'>
+                                    <div className="form-control w-full">
+                                        <label className="label">
+                                            <span className="label-text">Event Description</span>
+                                        </label>
+                                        <textarea cols='20' rows="3" name="description" className='border border-gray-300 rounded-lg p-2'></textarea>
+                                    </div>
+                                    <div className="form-control w-full">
+                                        <label className="label">
+                                            <span className="label-text">Upload Banner</span>
+                                        </label>
+                                        <input type="file" name="image"/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='flex gap-5'>
-                                <div className="form-control w-full">
-                                    <label className="label">
-                                        <span className="label-text">Event Description</span>
-                                    </label>
-                                    <input type="text" placeholder="email" className="input input-bordered" />
+                                <div className="form-control mt-6">
+                                    <button type='submit' className="btn btn-primary">Submit</button>
                                 </div>
-                                <div className="form-control w-full">
-                                    <label className="label">
-                                        <span className="label-text">Upload Banner</span>
-                                    </label>
-                                    <input type="file" />
-                                </div>
-                            </div>
-
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Submit</button>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
